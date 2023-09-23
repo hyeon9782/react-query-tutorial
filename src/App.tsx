0d3e62400610ec06dce750d2ff8013e431c7ddc0
hyeon9782 <hyeon9782@gmail.com>
2023-09-24 05:39:35 +0900

    feat: react query 기본 세팅 및 예시 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import HomePage from "./pages/HomePage";
 import SuperHeroesPage from "./pages/SuperHeroesPage";
 import RQSuperHeroesPage from "./pages/RQSuperHeroesPage";
 import Header from "./components/Header";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/super-heroes" element={<SuperHeroesPage />} />
-        <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/RQSuperHeroesPage.tsx
+++ src/pages/RQSuperHeroesPage.tsx
@@ -1,5 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const RQSuperHeroesPage = () => {
-  return <main>RQSuperHeroesPage</main>;
+  const { data, isLoading } = useQuery({
+    queryFn: () => {
+      return axios.get("http://localhost:4000/superheroes");
+    },
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <main>
+      <h1>RQSuperHeroesPage</h1>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </main>
+  );
 };
 
 export default RQSuperHeroesPage;
