c7d524fffad986da4534d5d031453ec547c3fc40
hyeon9782 <hyeon9782@gmail.com>
2023-09-24 06:48:10 +0900

    feat: cacheTime 과 staleTime

--- src/pages/RQSuperHeroesPage.tsx
+++ src/pages/RQSuperHeroesPage.tsx
@@ -6,10 +6,19 @@ const fetchSuperHeroes = () => {
 };
 
 const RQSuperHeroesPage = () => {
-  const { data, isLoading, isError, error } = useQuery({
+  // staleTime : 데이터가 만료되어 새로운 데이터를 다시 가져와야 하는 시간 (기본값 0)
+  // cacheTime : 쿼리의 데이터를 캐시로 저장하는 기간 (기본값 5분)
+  // RQ는 한 번 요청한 쿼리를 cache에 저장하기 하고 (기본 5분) cache에 저장된 쿼리는 서버에서서 가져오는
+  // 것이 아니라 cache에서 가져옵니다. (Network에서 fetch는 확인할 수 있다)
+  // 하지만 데이터 패칭을 자주할 필요가 없는 페이지인 경우 staleTime을 통해 데이터 패칭 자체를 일정 시간동안
+  // 안할 수 있수 있습니다.
+  const { data, isLoading, isError, error, isFetching } = useQuery({
     queryKey: ["super-heroes"],
     queryFn: fetchSuperHeroes,
+    staleTime: 30000,
   });
+
+  console.log({ isLoading, isFetching });
 
   if (isLoading) {
     return <h2>Loading...</h2>;
