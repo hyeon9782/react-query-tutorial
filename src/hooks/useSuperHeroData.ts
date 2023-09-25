0fa7d9eb950e9513cd5895b6a7007359627fd530
hyeon9782 <hyeon9782@gmail.com>
2023-09-26 01:51:04 +0900

    feat: query 기본 데이터 설정

--- src/hooks/useSuperHeroData.ts
+++ src/hooks/useSuperHeroData.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchSuperHero = ({ queryKey }) => {
@@ -8,8 +8,25 @@ const fetchSuperHero = ({ queryKey }) => {
 };
 
 export const useSuperHeroData = (heroId) => {
+  const queryClient = useQueryClient();
   return useQuery({
     queryKey: ["super-hero", heroId],
     queryFn: fetchSuperHero,
+    initialData: () => {
+      // queryClient에서 key값이 super-heroes인 query 값을 가져와서
+      // 해당 페이지의 데이터와 일치하는 데이터를 찾은 후에 기본값으로 설정
+      const hero = queryClient
+        .getQueryData(["super-heroes"])
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+      if (hero) {
+        return {
+          data: hero,
+        };
+      } else {
+        // 초기 데이터를 확보하는 것을 실패했을 경우 반드시 undefined를 넘겨줘야한다.
+        // 그렇지않으면 런타임 에러가 발생
+        return undefined;
+      }
+    },
   });
 };
